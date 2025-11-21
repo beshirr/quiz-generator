a4d9b66bd07b1e1a3122fd7f8419a7d5c4d8ffb8
beshirr <youssefahmedbeshir@gmail.com>
2025-11-21 19:55:55 +0200

    Routs

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,21 @@
-import { useState } from 'react'
 import './App.css'
 import { Routes, Route } from 'react-router-dom';
 import ClerkProviderRoutes from './auth/ClerkProviderRoutes';
+import { Layout } from './layout/Layout';
+import { AuthenticationPage } from './auth/AuthenticationPage';
+import { QuizGenerator } from './quiz/QuizGenerator';
+import { HistoryPanel } from './history/HistoryPanel';
 
 function App() {
   return (
     <ClerkProviderRoutes>
       <Routes>
-        
+        <Route path='/sign-in/*' element={<AuthenticationPage />} />
+        <Route path='/sign-up' element={<AuthenticationPage />} />
+        <Route element={<Layout />}>
+          <Route path='/' element={<QuizGenerator />} />
+          <Route path='/history' element={<HistoryPanel />} />
+        </Route>
       </Routes>
     </ClerkProviderRoutes>
   );
