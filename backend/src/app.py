1ac7d6ad152f8f553420ac2f9f5fccc0fb8d0ff6
beshirr <youssefahmedbeshir@gmail.com>
2025-11-22 14:48:23 +0200

    minor errot

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,11 +1,11 @@
-from fastapi import FastApi, Request, Response
+from fastapi import FastAPI, Request, Response
 from fastapi.middleware.cors import CORSMiddleware
 from clerk_backend_api import Clerk
 import os 
 
 clerk_sdk = Clerk(bearer_auth=os.getenv("CLERK_SECRET_KEY"))
 
-app = FastApi()
+app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware, 
